921cc907e3ad2af0648ad283a8c3a5e416657deb
uyhsp <uyhsp@student.kit.edu>
2019-08-14 13:28:38 +0200

    chapter2_programming_in_Linux

---
+++ chapter2_pragramming_basic/build/CMakeFiles/helloSALM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloSALM.dir/hello_world.cpp.o"
+  "helloSALM.pdb"
+  "helloSALM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloSALM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter2_pragramming_basic/libHelloSLAM.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+void printHello(){
+
+  cout << "Hello SLAM from a lib file"<<endl;
+
+
+}
+
